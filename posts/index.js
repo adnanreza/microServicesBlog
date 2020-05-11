9cb3bcd1933f8ba290cc3947ae4c7eed7a7d11a0
Adnan Reza <adnan.reza@outlook.com>
2020-05-11 16:30:56 -0700

    Received and broadcast events to all services

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,7 +24,7 @@ app.post('/posts', async (req, res) => {
     title,
   };
 
-  axios.post('http://localhost:4005/events', {
+  await axios.post('http://localhost:4005/events', {
     type: 'PostCreated',
     data: {
       id,
@@ -32,6 +33,11 @@ app.post('/posts', async (req, res) => {
   });
 
   res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received event', req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
